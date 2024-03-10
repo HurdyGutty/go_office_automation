8bca58fada117b1c3fa8c0d6797457230f22550f
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-03-10 21:13:47 +0700

    refactor: Make readExcel package

--- main.go
+++ main.go
@@ -2,63 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"time"
 
-	"github.com/xuri/excelize/v2"
+	"github.com/HurdyGutty/go_office_automation/pkg/readExcel"
 )
 
-type Person struct {
-	Name        string
-	DateOfBirth time.Time
-	Address     string
-	Phone       string
-}
-
 func main() {
-	f, err := excelize.OpenFile("../file_test/CV NHẤT.xlsx")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			fmt.Println(err)
-		}
-	}()
-
-	first_sheet := f.GetSheetName(0)
-
-	rows, err := f.GetRows(first_sheet, excelize.Options{RawCellValue: true})
-	if err != nil {
-		fmt.Println(err)
-	}
-	persons := []Person{}
-	for _, row := range rows[1:] {
-		person := Person{}
-		for i, colCell := range row {
-			switch i {
-			case 1:
-				float_date, err := strconv.ParseFloat(colCell, 64)
-				if err != nil {
-					fmt.Println(err)
-				}
-				time, err := excelize.ExcelDateToTime(float_date, false)
-				if err != nil {
-					fmt.Println(err)
-				}
-				person.DateOfBirth = time
-			case 0:
-				person.Name = colCell
-			case 2:
-				person.Address = colCell
-			case 3:
-				person.Phone = colCell
-			default:
-				fmt.Println("Error")
-			}
-		}
-		persons = append(persons, person)
-	}
-	fmt.Printf("%+v\n", persons)
+	persons := readExcel.ReadExcel()
+	fmt.Println(persons)
 }
