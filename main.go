7f092a11fc25c0180eef474b8c690f7bd7e66361
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-03-12 20:31:55 +0700

    refactor: Add workers

--- main.go
+++ main.go
@@ -4,11 +4,38 @@ import (
 	"fmt"
 
 	"github.com/HurdyGutty/go_office_automation/pkg/readExcel"
-	"github.com/HurdyGutty/go_office_automation/pkg/zipXML"
+	"github.com/HurdyGutty/go_office_automation/pkg/replace"
+	// "github.com/HurdyGutty/go_office_automation/pkg/zipXML"
 )
+
+func worker(persons []readExcel.Person, temp_dir string, jobs <-chan int, results chan<- int) {
+	for j := range jobs {
+		replace.Replace(persons[j], temp_dir, fmt.Sprintf("output/%d.docx", j+1))
+		results <- j
+	}
+}
 
 func main() {
 	persons := readExcel.ReadExcel()
-	fmt.Println(persons)
-	zipXML.ZipFile("word_xml\\", "output.docx")
+	length := len(persons)
+
+	var numJobs = length
+	temp_dir := "template/template_test.docx"
+
+	jobs := make(chan int, numJobs)
+	results := make(chan int, numJobs)
+
+	for w := 1; w <= 3; w++ {
+		go worker(persons, temp_dir, jobs, results)
+	}
+
+	for j := 0; j < numJobs; j++ {
+		jobs <- j
+	}
+	close(jobs)
+
+	for a := 0; a < numJobs; a++ {
+		<-results
+	}
+
 }
