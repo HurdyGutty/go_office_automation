abd0aa206d1f02793fd5cfb496649568a1b03adc
HurdyGutty <92365356+HurdyGutty@users.noreply.github.com>
2024-03-12 20:31:30 +0700

    Add: replace template

---
+++ pkg/replace/replace.go
@@ -0,0 +1,41 @@
+package replace
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/HurdyGutty/go_office_automation/pkg/readExcel"
+	"github.com/lukasjarosch/go-docx"
+)
+
+func Replace(person readExcel.Person, temp_dir, output_dir string) {
+	replaceMap := docx.PlaceholderMap{}
+
+	for k, v := range person {
+		len := len(k)
+		if t, ok := v.(time.Time); ok {
+			fmt.Println(t)
+			v = t.Format("02/01/2006")
+		}
+		replaceMap[k[1:len-1]] = v
+	}
+
+	fmt.Println(replaceMap)
+
+	doc, err := docx.Open(temp_dir)
+	if err != nil {
+		panic(err)
+	}
+
+	// replace the keys with values from replaceMap
+	err = doc.ReplaceAll(replaceMap)
+	if err != nil {
+		panic(err)
+	}
+
+	// write out a new file
+	err = doc.WriteToFile(output_dir)
+	if err != nil {
+		panic(err)
+	}
+}
